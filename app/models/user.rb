c5adf4b32377855329cc6811690ca6ea72a62a11
Ben Ettinger <ben.ettinger@gmail.com>
2013-09-29 20:21:21 -0400

    Added image to Post

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,9 @@ end
          :recoverable, :rememberable, :trackable, :validatable,:confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name,:avatar
   has_many :posts
   before_create :set_member
+  mount_uploader :avatar, AvatarUploader
   # attr_accessible :title, :body
 end
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,4 +1,4 @@
-if Rails.env.production?
+if Rails.env.development?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
@@ -8,5 +8,5 @@ if Rails.env.production?
     :password       => ENV['ppbtcurx'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
-  }
+  }  
 end
